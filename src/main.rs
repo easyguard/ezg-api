edad0373612e631318cd094840b382ce6310183b
Cfp <chaosflo44plus@gmail.com>
2024-06-17 16:52:07 +0200

    ✨ Create and delete firewall rules

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,28 @@ use std::path::Path;
 const EZG_ROOT: &str = "/etc/easyguard";
 
 #[derive(Debug, Deserialize)]
-struct Animal {
-	name: String,
-	legs: u16,
+struct FirewallRule {
+	port: u16,
+	protocol: String
+}
+
+#[derive(Debug, Deserialize)]
+struct FirewallPath {
+	zone: String,
+	chain: String,
+	rule: FirewallRule
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
 	let mut app = tide::new();
 	app.at("/api/firewall").get(get_firewall);
-	// app.at("/api/firewall/rule").put(put_firewall_rule);
+	app.at("/api/firewall/rule").put(put_firewall_rule);
+	app.at("/api/firewall/rule").delete(delete_firewall_rule);
 	app.at("/api/*").all(err404);
 
 	let cors = CorsMiddleware::new()
-    .allow_methods("GET, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+    .allow_methods("GET, PUT, DELETE, POST, OPTIONS".parse::<HeaderValue>().unwrap())
     .allow_origin(Origin::from("*"))
     .allow_credentials(false);
 	app.with(cors);
@@ -43,26 +51,46 @@ async fn get_firewall(mut req: Request<()>) -> tide::Result {
 }
 
 async fn put_firewall_rule(mut req: Request<()>) -> tide::Result {
-	let rule = req.body_string().await?; // { "zone": "zoneName", "chain": "chainName", "rule": {<object to add>} }
-	let rule_json: serde_json::Value = serde_json::from_str(&rule).expect("Invalid JSON 1");
-	let zone = rule_json["zone"].as_str().expect("Invalid JSON 2");
-	let chain = rule_json["chain"].as_str().expect("Invalid JSON 3");
-	let rule = rule_json["rule"].as_object().expect("Invalid JSON 4");
+	let FirewallPath { zone, chain, rule } = req.body_json().await?; // { "zone": "zoneName", "chain": "chainName", "rule": {<object to add>} }
 
-	let mut firewall: serde_json::Value = serde_json::from_str(&fs::read_to_string(Path::new(EZG_ROOT).join("firewall.json")).expect("Unable to read file")).expect("Invalid JSON 5");
-	// Find the zone and chain
-	// { "zones": [ { "name": "...", "<chains>": { "ports": [{...}, <add here>] } }, ... ] }
-	let zones = firewall["zones"].as_array_mut().expect("Invalid JSON 6");
-	let zone_index = zones.iter().position(|z| z["name"].as_str().expect("Invalid JSON") == zone).expect("Zone not found 7");
-	let chains = zones[zone_index][chain].as_array_mut().expect("Invalid JSON 8");
-	chains.push(serde_json::Value::Object(rule.clone()));
+	// run bash script to add rule
+	let output = std::process::Command::new("bash")
+		.current_dir(EZG_ROOT)
+		.arg(Path::new(EZG_ROOT).join("ezg").to_str().unwrap())
+		.arg("firewall")
+		.arg("rule")
+		.arg(zone)
+		.arg(chain)
+		.arg("add")
+		.arg(rule.protocol)
+		.arg(rule.port.to_string())
+		.output()
+		.expect("failed to execute process");
 
-	fs::write(Path::new(EZG_ROOT).join("firewall2.json"), serde_json::to_string_pretty(&firewall).expect("Invalid JSON 9")).expect("Unable to write file");
-
-	Ok("{\"success\": \"true\"}".into())
+	Ok("{\"success\": true}".into())
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-	let Animal { name, legs } = req.body_json().await?;
-	Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn delete_firewall_rule(mut req: Request<()>) -> tide::Result {
+	let FirewallPath { zone, chain, rule } = req.body_json().await?; // { "zone": "zoneName", "chain": "chainName", "rule": {<object to add>} }
+
+	// run bash script to add rule
+	let output = std::process::Command::new("bash")
+		.current_dir(EZG_ROOT)
+		.arg(Path::new(EZG_ROOT).join("ezg").to_str().unwrap())
+		.arg("firewall")
+		.arg("rule")
+		.arg(zone)
+		.arg(chain)
+		.arg("remove")
+		.arg(rule.protocol)
+		.arg(rule.port.to_string())
+		.output()
+		.expect("failed to execute process");
+
+	Ok("{\"success\": true}".into())
 }
+
+// async fn order_shoes(mut req: Request<()>) -> tide::Result {
+// 	let Animal { name, legs } = req.body_json().await?;
+// 	Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+// }
