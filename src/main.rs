7b9d64bdb963597ff88da2c6d939e387439ee838
Cfp <chaosflo44plus@gmail.com>
2024-06-30 19:26:47 +0200

    ✨ DNS

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,24 @@ struct FirewallPath {
 	rule: FirewallRule
 }
 
+#[derive(Debug, Deserialize)]
+struct DNSPatch {
+	option: String,
+	value: String
+}
+
 #[async_std::main]
 async fn main() -> tide::Result<()> {
 	let mut app = tide::new();
 	app.at("/api/firewall").get(get_firewall);
 	app.at("/api/firewall/rule").put(put_firewall_rule);
 	app.at("/api/firewall/rule").delete(delete_firewall_rule);
+	app.at("/api/dns").get(get_dns);
+	app.at("/api/dns").patch(patch_dns);
 	app.at("/api/*").all(err404);
 
 	let cors = CorsMiddleware::new()
-    .allow_methods("GET, PUT, DELETE, POST, OPTIONS".parse::<HeaderValue>().unwrap())
+    .allow_methods("GET, PUT, DELETE, POST, PATCH, OPTIONS".parse::<HeaderValue>().unwrap())
     .allow_origin(Origin::from("*"))
     .allow_credentials(false);
 	app.with(cors);
@@ -84,6 +92,29 @@ async fn delete_firewall_rule(mut req: Request<()>) -> tide::Result {
 		.arg("remove")
 		.arg(rule.protocol)
 		.arg(rule.port.to_string())
+		.output()
+		.expect("failed to execute process");
+
+	Ok("{\"success\": true}".into())
+}
+
+async fn get_dns(mut req: Request<()>) -> tide::Result {
+	let dns_text = fs::read_to_string(Path::new(EZG_ROOT).join("dns.json")).expect("Unable to read file");
+	println!("{}", dns_text);
+	Ok(dns_text.into())
+}
+
+async fn patch_dns(mut req: Request<()>) -> tide::Result {
+	let DNSPatch { option, value } = req.body_json().await?;
+
+	// run bash script to add rule
+	let output = std::process::Command::new("bash")
+		.current_dir(EZG_ROOT)
+		.arg(Path::new(EZG_ROOT).join("ezg").to_str().unwrap())
+		.arg("dns")
+		.arg("set")
+		.arg(option)
+		.arg(value)
 		.output()
 		.expect("failed to execute process");
 
